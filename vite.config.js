ee2c341a85156d275ba2219ac104ee02a30d5c6b
turaabhaider <turaab.haider0110@gmail.com>
2026-02-03 04:13:43 -0800

    origin

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,16 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  // This 'define' section fixes the C$1 error for Zoom
+  define: {
+    "process.env": {},
+    "global": "window", 
+  },
+  optimizeDeps: {
+    include: ['@zoomus/websdk', 'jsrsasign'],
+  },
   build: {
-    cssMinify: 'esbuild', // Faster and avoids Tailwind specific at-rule errors
+    cssMinify: 'esbuild',
     outDir: 'dist',
   }
 })
